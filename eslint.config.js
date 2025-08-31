ed1f6b68eed6a3d7ccf7f9016a4a459a318bb1d1
Pelle <52049523+farapholch@users.noreply.github.com>
2025-08-31 08:53:55 +0000

    Refactor ESLint configuration to use ES module syntax and integrate Prettier plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,33 @@
-const { FlatCompat } = require("@eslint/eslintrc");
-const typescriptPlugin = require("@typescript-eslint/eslint-plugin");
+// eslint.config.js
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
+import prettierPlugin from "eslint-plugin-prettier";
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: { eslint: true }, // <- ändrat från true
-});
-
-module.exports = [
-  ...compat.extends("eslint:recommended"),
+export default [
   {
+    // Basregler motsvarande eslint:recommended
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: "warn",
+    },
     plugins: {
       "@typescript-eslint": typescriptPlugin,
+      "prettier": prettierPlugin,
     },
     rules: {
-      "@typescript-eslint/no-unused-vars": "error",
+      // ESLint:recommended rules (viktigaste)
+      "no-unused-vars": "error",
+      "no-console": "warn",
+      "eqeqeq": ["error", "always"],
+
+      // TypeScript regler
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
       "@typescript-eslint/no-explicit-any": "warn",
+
+      // Prettier
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
     },
   },
 ];
