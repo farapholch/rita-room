6b16fd81e9c0074470b9cca6bc40231bd8e09d61
Pelle <52049523+farapholch@users.noreply.github.com>
2025-08-31 08:55:50 +0000

    Refactor ESLint configuration to improve parser setup and streamline rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,21 @@
-// eslint.config.js
+import typescriptParser from "@typescript-eslint/parser";
 import typescriptPlugin from "@typescript-eslint/eslint-plugin";
 import prettierPlugin from "eslint-plugin-prettier";
 
 export default [
   {
-    // Basregler motsvarande eslint:recommended
     languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
-    },
-    linterOptions: {
-      reportUnusedDisableDirectives: "warn",
+      parser: typescriptParser, // <-- parser för TypeScript
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+      },
     },
     plugins: {
       "@typescript-eslint": typescriptPlugin,
-      "prettier": prettierPlugin,
+      prettier: prettierPlugin,
     },
     rules: {
-      // ESLint:recommended rules (viktigaste)
-      "no-unused-vars": "error",
-      "no-console": "warn",
-      "eqeqeq": ["error", "always"],
-
-      // TypeScript regler
-      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-      "@typescript-eslint/no-explicit-any": "warn",
-
-      // Prettier
       "prettier/prettier": ["error", { endOfLine: "auto" }],
-    },
-  },
-];
+      eqeqeq: ["error", "always"],
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }]_]()
