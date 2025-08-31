557796800e0c3282f07731d3730b6d9c95f5bc6b
Pelle <52049523+farapholch@users.noreply.github.com>
2025-08-31 08:50:40 +0000

    Enhance ESLint configuration with recommended settings and additional TypeScript rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,11 @@
 const { FlatCompat } = require("@eslint/eslintrc");
 const typescriptPlugin = require("@typescript-eslint/eslint-plugin");
+const path = require("path");
 
-const compat = new FlatCompat({ baseDirectory: __dirname });
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: true, // <-- viktigt
+});
 
 module.exports = [
   ...compat.extends("eslint:recommended"),
@@ -11,6 +15,7 @@ module.exports = [
     },
     rules: {
       "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
     },
   },
 ];
